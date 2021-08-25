a4f4a3c5b56d2e11fcc020bfeebbb5e5214ba8db
Santiago Ramirez <sa.ramirezr@uniandes.edu.co>
2021-08-24 23:48:36 -0500

    Entrega Final - laboratorio 2

--- App/view.py
+++ App/view.py
@@ -35,6 +35,7 @@ La vista se encarga de la interacción con el usuario
 Presenta el menu de opciones  y  por cada seleccion
 se hace la solicitud al controlador para ejecutar la
 operación solicitada
+
 """
 
 
